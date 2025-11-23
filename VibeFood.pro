e615e49732f44b1cd1c68483b5172d91a3e96af4
eloranta <hannu.eloranta@gmail.com>
2025-11-23 18:26:31 +0200

    starting over again

--- VibeFood.pro
+++ VibeFood.pro
@@ -9,19 +9,16 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    IngredientModelWithCheck.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    IngredientModelWithCheck.h \
     mainwindow.h
 
 FORMS += \
     mainwindow.ui
 
-TRANSLATIONS += \
-    VibeFood_en_US.ts
+TRANSLATIONS +=
 CONFIG += lrelease
 CONFIG += embed_translations
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,17 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QSqlDatabase>
-
-class QSqlTableModel;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
-
-class IngredientModelWithCheck;
 
 class MainWindow : public QMainWindow
 {
@@ -23,22 +18,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QSqlDatabase db;
-    QSqlTableModel *foodModel;
-    IngredientModelWithCheck *ingredientsModel;
-
-    bool openDatabase();
-    bool createTables();
-    void seedDataIfEmpty();
-    void setupModelAndView();
-    void addItem(const QString &food, const QString &ingredient, const QString &amount);
 
 private slots:
-    void onFoodSelectionChanged(const QModelIndex &current, const QModelIndex &previous);
-    void onShowAllToggled(bool checked);
-    void onRecipeChanged();
-    void onAddFoodClicked();
-    void onDeleteFoodClicked();
+
 };
 
 #endif // MAINWINDOW_H
